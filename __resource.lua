7c0ab862d5825e50995b0860fc274f7b6971018b
samuelds <samuelds.pro@gmail.com>
2017-08-09 02:45:49 +0200

    Removing Player Management

--- __resource.lua
+++ __resource.lua
@@ -9,18 +9,14 @@ client_scripts {
 
   "configurations.lua", -- Configuration file
   "client/cl_init.lua", -- Init Client
-  "client/cl_player.lua", -- Player
   "client/cl_playerEvents.lua", -- Player events
 
 }
 
 server_scripts {
 
-  "@mysql-async/lib/MySQL.lua", -- Load mysql-async
   "configurations.lua", -- Configuration file
   "server/sv_utils.lua", -- Utils
-  "server/sv_player.lua", -- Player manager
-  "server/sv_players.lua", -- Players managers
   "server/sv_commands.lua", -- Commands mananagers
   "server/sv_init.lua", -- Init Server
 
@@ -28,16 +24,9 @@ server_scripts {
 
 exports {
 
-  "SetPlayerData",
-  "GetPlayerData",
-
 }
 
 server_exports {
 
   "AddCommand",
-  "GetPlayers",
-  "GetPlayer",
-  "PlayerExist",
-
 }
