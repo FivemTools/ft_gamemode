35bd28bab28f4d1af1db26f762f036a5a8f1fa96
samuelds <samuelds.pro@gmail.com>
2017-08-13 13:35:51 +0200

    Comment load code

--- __resource.lua
+++ __resource.lua
@@ -3,30 +3,25 @@
 
 resource_type "gametype" { name = "FiveMTools v0.14.1" }
 
-resource_manifest_version "77731fab-63ca-442c-a67b-abc70f28dfa5"
+dependencies {
+  "ft_players",
+}
 
 client_scripts {
-
   "configurations.lua", -- Configuration file
   "client/cl_init.lua", -- Init Client
   "client/cl_playerEvents.lua", -- Player events
-
 }
 
 server_scripts {
-
-  "configurations.lua", -- Configuration file
-  "server/sv_utils.lua", -- Utils
-  "server/sv_commands.lua", -- Commands mananagers
-  "server/sv_init.lua", -- Init Server
-
+  -- "configurations.lua", -- Configuration file
+  -- "server/sv_utils.lua", -- Utils
+  -- "server/sv_commands.lua", -- Commands mananagers
+  -- "server/sv_init.lua", -- Init Server
 }
 
-exports {
-
-}
+exports {}
 
 server_exports {
-
-  "AddCommand",
+  -- "AddCommand",
 }
--- client/cl_playerEvents.lua
+++ client/cl_playerEvents.lua
@@ -14,50 +14,50 @@ AddEventHandler("playerSpawned", function()
   end)
 end)
 
-AddEventHandler("ft_gamemode:ClFirstJoinProper", function()
-  Citizen.CreateThread(function()
-
-    while true do
-      Citizen.Wait(0)
-
-      local playerId = PlayerId()
-
-      if Config.traffic ~= nil and Config.traffic >= 0.0 and Config.traffic <= 1.0 then
-        if Config.traffic == 1.0 then
-          Config.traffic = 0.9999999
-        end
-        SetVehicleDensityMultiplierThisFrame(Config.traffic)
-      end
-
-      if Config.pedDensity ~= nil and Config.pedDensity >= 0.0 and Config.pedDensity <= 1.0 then
-        if Config.pedDensity == 1.0 then
-          Config.pedDensity = 0.9999999
-        end
-        SetPedDensityMultiplierThisFrame(Config.pedDensity)
-      end
-
-      if Config.everyIgnores ~= nil and Config.everyIgnores then
-        SetEveryoneIgnorePlayer(playerId, true)
-      end
-
-      if Config.policeIgnores ~= nil and Config.policeIgnores then
-        SetPoliceIgnorePlayer(playerId, true)
-        SetDispatchCopsForPlayer(playerId, false)
-      end
-
-      if Config.noWantedLevel ~= nil and Config.noWantedLevel then
-        SetMaxWantedLevel(0)
-      end
-
-      if Config.vehicleRewards ~= nil and Config.vehicleRewards then
-        DisablePlayerVehicleRewards(playerId)
-      end
-
-      if Config.pedScenario ~= nil and Config.pedScenario then
-	     SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
-      end
-
-    end
-
-  end)
-end)
+-- AddEventHandler("ft_gamemode:ClFirstJoinProper", function()
+--   Citizen.CreateThread(function()
+--
+--     while true do
+--       Citizen.Wait(0)
+--
+--       local playerId = PlayerId()
+--
+--       if Config.traffic ~= nil and Config.traffic >= 0.0 and Config.traffic <= 1.0 then
+--         if Config.traffic == 1.0 then
+--           Config.traffic = 0.9999999
+--         end
+--         SetVehicleDensityMultiplierThisFrame(Config.traffic)
+--       end
+--
+--       if Config.pedDensity ~= nil and Config.pedDensity >= 0.0 and Config.pedDensity <= 1.0 then
+--         if Config.pedDensity == 1.0 then
+--           Config.pedDensity = 0.9999999
+--         end
+--         SetPedDensityMultiplierThisFrame(Config.pedDensity)
+--       end
+--
+--       if Config.everyIgnores ~= nil and Config.everyIgnores then
+--         SetEveryoneIgnorePlayer(playerId, true)
+--       end
+--
+--       if Config.policeIgnores ~= nil and Config.policeIgnores then
+--         SetPoliceIgnorePlayer(playerId, true)
+--         SetDispatchCopsForPlayer(playerId, false)
+--       end
+--
+--       if Config.noWantedLevel ~= nil and Config.noWantedLevel then
+--         SetMaxWantedLevel(0)
+--       end
+--
+--       if Config.vehicleRewards ~= nil and Config.vehicleRewards then
+--         DisablePlayerVehicleRewards(playerId)
+--       end
+--
+--       if Config.pedScenario ~= nil and Config.pedScenario then
+-- 	     SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
+--       end
+--
+--     end
+--
+--   end)
+-- end)
